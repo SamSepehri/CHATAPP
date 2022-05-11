cc0186dd7098de9135fe8524deee43419fb3839c
samisep <sam.sepehri@gmx.de>
2022-05-11 15:07:31 +0200

    Create Chat.js

---
+++ components/Chat.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+
+export default class Chat extends React.Component {
+    render() {
+        let name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name })
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Button
+                    title="Go to Start"
+                    onPress={() => this.props.navigation.navigate('Start')}
+                />
+            </View>
+        )
+    }
+}
