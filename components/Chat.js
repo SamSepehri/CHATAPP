1b57d6342baab91ce39c83e2de3f585fa4baf6d9
samisep <sam.sepehri@gmx.de>
2022-05-11 16:53:42 +0200

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-export default class Chat extends React.Component {
-    render() {
-        let name = this.props.route.params.name;
-        this.props.navigation.setOptions({ title: name })
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Button
-                    title="Go to Start"
-                    onPress={() => this.props.navigation.navigate('Start')}
-                />
-            </View>
-        )
+
+export default function Chat(props) {
+
+    // Retrieving the name and color properties passed from the Start Screen
+    let { name, color } = props.route.params;
+    useEffect(() => {
+
+        // Set the screen title to the user name entered in the start screen
+        props.navigation.setOptions({ title: name });
     }
+    )
+
+    return (
+        // Setting the background color to the color picked by the user in the start screen
+        <View style={[{ backgroundColor: color }, styles.container]}>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
