bd4e74b1042c6af52860cf5b0491cfe32bdd5180
samisep <sam.sepehri@gmx.de>
2022-05-12 18:59:05 +0200

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,74 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { View, Platform, KeyboardAvoidingView } from "react-native";
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 
-export default function Chat(props) {
-
-    // Retrieving the name and color properties passed from the Start Screen
-    let { name, color } = props.route.params;
-    useEffect(() => {
-
-        // Set the screen title to the user name entered in the start screen
-        props.navigation.setOptions({ title: name });
+export default class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        };
     }
-    )
 
-    return (
-        // Setting the background color to the color picked by the user in the start screen
-        <View style={[{ backgroundColor: color }, styles.container]}>
-        </View>
-    )
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+            ],
+        })
+    }
+
+    // this is what will be called when a user sends a message
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    renderBubble(props) {
+        return (
+            <Bubble {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: '#000',
+                    },
+                }}
+            />
+        );
+    }
+
+    render() {
+
+        let name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name });
+
+        // takes the input parameters of background color defined in the start.js component
+        let bgColor = this.props.route.params.bgColor;
+        return (
+            <View style={{ flex: 1 }}>
+                <GiftedChat
+                    renderBubble={this.renderBubble.bind(this)}
+                    messages={this.state.messages}
+                    onSend={(messages) => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
+                />
+
+                {/* this fixes android keyboard */}
+                {Platform.OS === "android" ? (<KeyboardAvoidingView behavior="height" />) : null}
+            </View>
+
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-})
