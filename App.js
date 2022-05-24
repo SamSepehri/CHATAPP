5293d368e092b54f95a8489cbf56e81061253d38
samisep <sam.sepehri@gmx.de>
2022-05-24 15:57:38 +0200

    update

--- App.js
+++ App.js
@@ -1,27 +1,78 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, Alert, ScrollView } from 'react-native';
-import Chat from './components/Chat';
+import React, { useState, createContext, useContext, useEffect } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './config/firebase';
+
+// import the screens
 import Start from './components/Start';
+import Chat from './components/Chat';
+
+// import react native gesture handler
 import 'react-native-gesture-handler';
 
-// import react navigation
+// import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-//Create the navigator
+// Create the navigator
 const Stack = createStackNavigator();
 
-export default class App extends React.Component {
+// Create a contect for the user data
+const AuthenticatedUserContext = createContext({});
 
-  render() {
+// Create a Provider function to allow screen components to acces the current user
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
+
+function RootNavigator() {
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // onAuthStateChanges returns an unsubscriber
+    const unsubscribeAuth = onAuthStateChanged(
+      auth,
+      async authenticatedUser => {
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+        setIsLoading(false);
+      }
+    );
+
+    // unsubscribe auth listener to unmount
+    return unsubscribeAuth;
+  }, [user]);
+
+  if (isLoading) {
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Start'>
-          <Stack.Screen name='Start' component={Start} />
-          <Stack.Screen name='Chat' component={Chat} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
     );
   }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Start'>
+        <Stack.Screen name='Chat' component={Chat} />
+        <Stack.Screen name='Start' component={Start} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default function App() {
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
+  );
+}
+
